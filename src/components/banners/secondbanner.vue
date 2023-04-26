ed8ce1fd6c37652b0f988aa27e8627d34110ffb7
sekani50 <mohammedadeoye0@gmail.com>
2023-04-26 13:17:19 -0700

    secbandone

--- src/components/banners/secondbanner.vue
+++ src/components/banners/secondbanner.vue
@@ -4,10 +4,11 @@
        
             <img class="w-full h-full object-fill" src="@/assets/images/secban.png" alt="" />
 
-        <div class="relative mx-auto">
-            <p class="absolute">Special Offer</p>
-        <p class="absolute">Unisex Bespoke</p>
-        </div>
+        <div class="absolute inset-0 w-full h-fit m-auto">
+            <p class="text-center special font-thin text-4xl sm:text-6xl xl:text-7xl">Special Offer</p>
+        <p class="text-center collectio uppercase text-[16px]  sm:text-2xl font-bold">Unisex Bespoke</p>
+            <p class="text-center uppercase text-[11px] sm:text-[16px]  mt-3 sm:mt-6"><span class="font-semibold text-gray-200 bg-black p-1 rounded-sm">chat with us</span></p>
+    </div>
        
   </div>
 </template>
